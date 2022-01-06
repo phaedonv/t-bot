2b974826a0eb50a2613070ded247acec14ea768e
phaedonv <faival@protonmail.com>
2022-01-06 14:35:13 +0100

    added some testing code to verify functionality

--- twitter.py
+++ twitter.py
@@ -8,6 +8,6 @@ auth.set_access_token('1469326097607278600-9Mt9WfptbOhNH6lASOWmv9NyDtYg54','GWeM
 api = tweepy.API(auth, wait_on_rate_limit=True) # tweeky removed 'wait_on_rate_limit_notify=True' parameter
 
 #screen_name = "TheArtOfFreedom"
-user = api.get_user(screen_name='ArtOFreedom_NFT')
+user = api.get_user(screen_name='ArtOFreedom_NFT') #actually the screen_name is the Twitter username without the @
 
 print('name: ' + user.screen_name + ' :: ' + 'ID: ' + str(user.id))
