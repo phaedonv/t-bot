d042b3bbe77f30ed76d48bd3278c6d4714801960
phaedonv <faival@protonmail.com>
2022-01-06 14:15:49 +0100

    getting user screen_name

--- twitter.py
+++ twitter.py
@@ -5,6 +5,9 @@ auth = tweepy.OAuthHandler('v1wPEFI9mqKdvigCcqQta8kih','7QsAEtgPnxHyvAOi4DyfFN6f
 
 auth.set_access_token('1469326097607278600-9Mt9WfptbOhNH6lASOWmv9NyDtYg54','GWeMQocAfY5u3rhfDPtEUJPTGa13oVm2TrlXeAtqCP2Rv')
 
-api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+api = tweepy.API(auth, wait_on_rate_limit=True) # tweeky removed 'wait_on_rate_limit_notify=True' parameter
 
+#screen_name = "TheArtOfFreedom"
+user = api.get_user(screen_name='ArtOFreedom_NFT')
 
+print(user)
