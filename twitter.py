4a1f03233ff7d4ac038ee5c89702e985ca2f2a62
phaedonv <faival@protonmail.com>
2022-01-19 19:28:24 +0100

    tweet create-destroy

--- twitter.py
+++ twitter.py
@@ -14,6 +14,26 @@ api = tweepy.API(auth, wait_on_rate_limit=True) # tweeky removed 'wait_on_rate_l
 #screen_name = "TheArtOfFreedom"
 user = api.get_user(screen_name='ArtOFreedom_NFT') #actually the screen_name is the Twitter username without the @
 
-print('name: ' + user.screen_name + ' :: ' + 'ID: ' + str(user.id)) #just testing ;)
+#print('name: ' + user.screen_name + ' :: ' + 'ID: ' + str(user.id)) #just testing ;)
+
+#let's create a tweet!
+
+tweet_text = 'Work in progress!'
+media_nft = 'media/sound_of_da_police.png'
+
+#status = api.update_status_with_media(tweet_text, media_nft)
+#print(status)
+
+#I have to find a way of getting the status-tweet IDs..
+#The ID of last tweet is known : 1483865642285551620
+
+#let's try to delete a tweet! 
+ID = 1483865642285551620
+api.destroy_status(ID)
+
+try:
+    api.get_status(ID)
+except:
+    print('The tweet has beed deleted😎')
 
    
