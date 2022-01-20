d96fef7e768b37daae54314d13aec40d8a536f6f
phaedonv <faival@protonmail.com>
2022-01-20 19:54:57 +0100

    entities

--- like.py
+++ like.py
@@ -1,5 +1,7 @@
 #favorite tweet based on search term
 
+from tkinter import N
+from turtle import end_fill
 import tweepy
 from keys import keys
 import time
@@ -18,11 +20,16 @@ user = api.get_user(screen_name='ArtOFreedom_NFT') #actually the screen_name is 
 
 search_term = 'nft' or '#nft' or 'nftartwork' or 'nftcommunity' or 'cryptoart'
 nrTweets = 500
-
+        
 for tweet in tweepy.Cursor(api.search_tweets, search_term, result_type='recent', lang="en").items(nrTweets):
     try:
         tweet.favorite()
-        print(' Tweet Liked')
+
+        #I have to find a way to print the name of the user that tweeted.. 
+        #entities example: entities = {'hashtags': [], 'symbols': [], 'user_mentions': [{'screen_name': 'BrettLeeWEB3', 'name': 'Brett Lee ♤', 'id': 1448190946688991234, 'id_str': '1448190946688991234', 'indices': [3, 16]}], 'urls': []}
+        
+        #print(tweet.entities[user_mentions]([name]))
+        print('  Tweet ',tweet.id, ' Liked')
         time.sleep(10)
     #except tweepy.errors.TweepError as e:
         #print(e.reason)
