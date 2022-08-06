ebd48951149e7e5e3b26e6de49396cca1074412b
phaedonv <faival@protonmail.com>
2022-08-06 09:30:45 +0200

    tweet_reply

---
+++ tweet_reply.py
@@ -0,0 +1,19 @@
+#Reply to tweet based on search term
+
+from config import LIKE
+
+import tweepy
+from keys import keys
+import time
+
+API_KEY = keys['api_key']
+API_SECRET = keys['api_secret']
+ACCESS_TOKEN = keys['access_token']
+ACCESS_TOKEN_SECRET = keys['access_token_secret']
+
+auth = tweepy.OAuthHandler(API_KEY,API_SECRET) #api key and secret
+auth.set_access_token(ACCESS_TOKEN,ACCESS_TOKEN_SECRET) #access token and secret
+api = tweepy.API(auth, wait_on_rate_limit=True) # tweeky removed 'wait_on_rate_limit_notify=True' parameter
+
+#screen_name = "TheArtOfFreedom"
+user = api.get_user(screen_name='ArtOFreedom_NFT') #actually the screen_name is the Twitter username without the @
